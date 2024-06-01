0484108e13c085d15efd8bfd10a491afbc906a2e
Tephenss <159686640+Tephenss@users.noreply.github.com>
2024-06-01 23:27:02 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Student from LAGUNA UNIVERSITY" , "IT Programmer" , "Student of Sir Jhonie" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
